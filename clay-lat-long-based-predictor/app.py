4aab0acb5c0e52f26e6380e6877b79c78692aaab
Parth Shukla <shuklaparth501@gmail.com>
2022-07-08 18:08:39 +0530

    Update app.py

--- clay-lat-long-based-predictor/app.py
+++ clay-lat-long-based-predictor/app.py
@@ -6,7 +6,7 @@ import numpy as np
 
 
 pipe = pickle.load(open('clay-latlong-small.pkl','rb'))
-st.title('Clay Predictor')
+st.title('Lat - Long Based Clay Predictor')
 
 
 coarse = st.number_input('coarse')
@@ -26,5 +26,5 @@ GPS_LONG = st.number_input('GPS_LONG')
 if st.button('Predict Clay'):
       input=pd.DataFrame({'coarse':[coarse],'pH_in_H2O':[pH_in_H2O],'pH_in_CaCl2':[pH_in_CaCl2],'OC':[OC],'CaCO3':[CaCO3],'N':[N],'P':[P],'K':[K],'CEC':[CEC],'GPS_LAT':[GPS_LAT],'GPS_LONG':[GPS_LONG]})
       result = pipe.predict(input)
-      st.success('THE Clay FOR GIVEN DATA WILL BE {}'.format(result))
+      st.success('THE CLAY FOR GIVEN DATA WILL BE {}'.format(result))
 # st.header(result)
